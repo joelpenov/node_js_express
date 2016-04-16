021cf2eebaf6645b68a62632f4787100a0ec8e74
Joel Peña Arias <jukey09@gmail.com>
2016-04-16 16:25:13 -0400

    Routing books

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static('static'));
 app.set(('./src/views'));
 app.set('views', './src/views');
 
+var booksRouter = express.Router();
 
 // var habdlebars = require('express-handlebars');
 // app.engine('.hbs', habdlebars({extname: '.hbs'}));
@@ -16,8 +17,23 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 
+booksRouter.route('/')
+.get( function(request, response) {
+	response.send('/books');
+});
+
+booksRouter.route('/single')
+.get( function(request, response) {
+	response.send('/books/single');
+});
+
+app.use('/books', booksRouter);
+
 app.get('/', (request, response) =>{
-    response.render('index', {title: 'Title from render', patriots: ['Juan Pablo Duarte', 'Ramon Matias Mella', 'Francisco del Rosario Sanchez']});
+    response.render('index', {title: 'Title from render', menu: [
+    	{link:'/books', label:'Books'},
+    	{link:'/authors', label:'Authors'}
+    	]});
 });
 
 app.get('/books', (request, response) =>{
