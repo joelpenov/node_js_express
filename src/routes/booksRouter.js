1cdbb0bbca32a4eca83788e819925f52a6c87aac
Joel Peña Arias <jukey09@gmail.com>
2016-04-18 22:07:22 -0400

    Get data from MongoDB

--- src/routes/booksRouter.js
+++ src/routes/booksRouter.js
@@ -1,54 +1,40 @@
 
 var express = require('express');
-var mysql = require('mysql');
+var mongodb = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+var mongoDbUrl = 'mongodb://localhost:27017/books';
 
 var booksRouter = express.Router();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "booksuser",
-  password: "W3lc0m3Job",
-  database: "booksdb"
-});
-
-
-connection.connect(function(err){
- if(!err) {
-     console.log("Database is connected ... \n\n");  
- } else {
-     console.log("Error connecting database ... \n\n", err);  
- }
- });
-
 var router = function (menu) {
-	booksRouter.route('/')	
+	booksRouter.route('/')
 	.get( function(request, response) {
-		connection.query('SELECT * from book;', function(err, rows, fields) {
- 		
-	   if (!err)
-	     response.render('books', {title:'Books', books: rows, menu: menu});
-	   else
-	     console.log('Error while performing Query.');
-	   });		
+		
+		mongodb.connect(mongoDbUrl, function(err, db){
+			var books = db.collection('book');
+			books.find({}).toArray(function(error, bookObjects){								
+				response.render('books', {title:'Books', books: bookObjects, menu: menu});		
+			});
+			
+		});
+	     
 	});
 
 	booksRouter.route('/:id')
 	.all(function(req, resp, next){
 		var id = req.params.id;
-		var query = `SELECT * from book where id = ${id};`;		
-		connection.query(query, function(err, rows, fields) {		
-
-	   if (!err){	   	
-	   		req.book = rows[0];
-	   		next();
-	   }	   
-	   else
-	     console.log('Error while performing Query.');
-	   });	
-		
+		mongodb.connect(mongoDbUrl, function(err, db){
+			var books = db.collection('book');			
+			books.findOne({_id:  new ObjectId(id)}, function(error, bookObject){				
+				req.book = bookObject;				
+	   			next();		
+			});
+			
+		});	   		
 	})
 	.get( function(request, response) {
-		var book = request.book || {};
+		var book = request.book || {};		
 		response.render('book', {title: book.title ? book.title + ' - ' + book.author: "Not found", menu: menu, book: book});
 	});
 
